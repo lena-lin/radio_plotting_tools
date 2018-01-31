6b54d932d46355aef79d966051059f19b98d8f9c
llinhoff <lena.linhoff@tu-dortmund.de>
2018-01-31 15:23:03 +0100

    add loglevs option

--- radio_plotting_tools/scripts/plot_clean_map.py
+++ radio_plotting_tools/scripts/plot_clean_map.py
@@ -1,7 +1,6 @@
 import click
 import numpy as np
 from astropy.io import fits
-import astropy.units as u
 
 
 import matplotlib.pyplot as plt
@@ -12,18 +11,26 @@ from ..coordinates import get_pixel_coordinates
 
 @click.command()
 @click.argument('input_file', type=click.Path(file_okay=True, dir_okay=False))
+@click.option(
+    '-loglevs',
+    type=click.FLOAT,
+    default=0.02,
+)
 def main(
     input_file,
+    loglevs,
 ):
     difmap_data = fits.open(input_file)
     clean_map = difmap_data['PRIMARY'].data[0][0]
 
+    flux_min = clean_map.min()
     flux_max = clean_map.max()
+    loglevs_min = flux_min + (flux_max - flux_min) * loglevs
     x, y = get_pixel_coordinates(difmap_data['PRIMARY'].header)
 
     fig, ax = plt.subplots()
     ax.set_aspect(1)
-    ax.contour(x, y, clean_map, norm=LogNorm(), levels=np.logspace(np.log10(flux_max*1e-2), np.log10(flux_max), 10))
+    ax.contour(x, y, clean_map, norm=LogNorm(), levels=np.logspace(np.log10(loglevs_min), np.log10(flux_max), 10))
     ax.set_xlabel('Relative Right Ascension in mas')
     ax.set_ylabel('Relative Declination in mas')
     plt.show()
