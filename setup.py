258a59f0344e959edceb6ad667fc422c82be5bca
llinhoff <lena.linhoff@tu-dortmund.de>
2018-01-31 13:08:21 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='radio_plotting_tools',
+    author='Lena Linhoff, Kevin Schmidt',
+    author_email='lena.linhoff@tu-dortmund.de',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'astropy',
+        'numpy',
+        'matplotlib',
+        'click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'radio_plotting_clean_map = radio_plotting_tools.scripts.plot_clean_map:main',
+        ]
+    }
+)
