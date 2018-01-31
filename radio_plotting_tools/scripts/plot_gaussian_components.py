8c78e1ad31461b9e5eea6be0440e799894c139b4
Kevin Schmidt <kevin3.schmidt@udo.edu>
2018-01-31 17:15:36 +0100

    add linear regression for c1 component

--- radio_plotting_tools/scripts/plot_gaussian_components.py
+++ radio_plotting_tools/scripts/plot_gaussian_components.py
@@ -9,7 +9,7 @@ from astropy.io import fits
 import astropy.units as u
 import glob
 from matplotlib.colors import LogNorm
-
+from scipy.optimize import curve_fit
 
 
 datapaths = ([])
@@ -25,8 +25,20 @@ def time_difference(dates, shortest_difference, scaling):
 
 fig = plt.figure(figsize=(12,20))
 ax = fig.add_subplot(111, aspect='equal')
+
+# Define Empty Arrays
 num_epoch = 0
 y_ticks = ([])
+x_core = ([])
+y_core = ([])
+x_c1 = ([])
+y_c1 = ([])
+
+# Define Linear Function
+def linear_fit(f, m, b):
+    return m * f + b
+
+# Start Iteration Through Different Epochs
 
 for epoch in sorted(datapaths):
 
@@ -72,27 +84,47 @@ for epoch in sorted(datapaths):
     y = y.value - time_diff[num_epoch]
 
     # Plot Data
-    plt.plot(delta_x, delta_y,  marker='.', markersize= 3, linestyle='none', label='Gaussian Components')
+    plt.plot(delta_x, delta_y, color='black',  marker='.', markersize= 2, linestyle='none')
 
     ax.set_aspect(1)
     ax.contour(x, y, clean_map, norm=LogNorm(), levels=np.logspace(np.log10(loglevs_min), np.log10(flux_max), 10))
 
     # Plot Ellipses
     for i in range(len(delta_x)):
-        ellipse = Ellipse((delta_x[i].value, delta_y[i]), height=major_ax[i].value, width=minor_ax[i].value, angle=phi_ellipse[i], edgecolor='black', facecolor='none')
-        #ax.axis('equal')
+        ellipse = Ellipse((delta_x[i].value, delta_y[i]), height=major_ax[i].value, width=minor_ax[i].value, angle=phi_ellipse[i], edgecolor='black', facecolor='none', linewidth=0.5)
         ax.add_artist(ellipse)
+
+    # Generate Arrays For Same Corecomponents
+    x_core = np.append(x_core, delta_x[0].value)
+    y_core = np.append(y_core, delta_y[0])
+    x_c1 = np.append(x_c1, delta_x[1].value)
+    y_c1 = np.append(y_c1, delta_y[1])
+
 
     # Generate Y_Ticks
     y_ticks = np.append(y_ticks ,delta_y[0])
     num_epoch += 1
 
+x = np.linspace(-40, 20, 10)
+# Line Core
+plt.plot(x_core, y_core, color='red', marker='+', label='Core', linestyle='none', markersize=5)
+plt.axvline(0, ymin=0, ymax=1, color='red', linewidth=1)
+
+# Linear Fit C1
+print(x_c1)
+print(y_c1)
+params, covariance = curve_fit(linear_fit, x_c1, y_c1, p0=[-100000, 10000000])
+errors = np.sqrt(np.diag(covariance))
+plt.plot(x_c1, y_c1, color='blue', marker='+', label='C1', linestyle='none', markersize=5)
+plt.plot(x, linear_fit(x,*params), color='blue', linewidth=1)
+print(params[0], params[1])
+
 plt.yticks(y_ticks, dates)
 plt.xlabel('Relative RA / mas')
-#plt.legend()
+plt.legend()
 
 plt.xlim(5, -15)
-plt.ylim(-40, 5)
+plt.ylim(-35, 3)
 plt.tight_layout()
 #plt.show()
 plt.savefig('dynamic_plot.pdf', bbox_inches='tight', pad_inches=0.1)
