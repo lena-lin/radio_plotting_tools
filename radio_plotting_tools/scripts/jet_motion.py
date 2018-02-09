7a883defc999e1d7c04eb3a18aa12e7a8e210258
Kevin Schmidt <kevin3.schmidt@udo.edu>
2018-02-09 14:22:26 +0100

    fixed datetime issue

--- radio_plotting_tools/scripts/jet_motion.py
+++ radio_plotting_tools/scripts/jet_motion.py
@@ -33,13 +33,11 @@ delta_days = ((date - date.min()) / np.timedelta64(1,'D'))
 delta_days = pd.DataFrame({'delta_days': delta_days})
 df_components = pd.concat([df_components, delta_days], axis=1)
 
-
-
 # Define linear function
 def linear_fit(f, m, b):
     return m * f + b
 
-x_values = np.linspace(16000, 17000, 1000)
+x_values = np.linspace(16000, 17000, 10)
 
 # Colors for different c_i
 colors = (['red', 'blue', 'magenta', 'orange', 'green', 'brown', 'cyan', 'darkorange', 'grey', 'olive'])
@@ -59,32 +57,25 @@ for i in range(len(df_components.loc[df_components['date'] == '2013-12-15', 'x_p
              label='C_'+str(i),
              color=colors[i]
              )
+    print(df_components.loc[df_components['c_i'] == i, 'delta_days'])
+    print(df_components.loc[df_components['c_i'] == i, 'radial_dist'])
+    params, covariance = curve_fit(linear_fit,
+                     df_components.loc[df_components['c_i'] == i, 'delta_days'],
+                     df_components.loc[df_components['c_i'] == i, 'radial_dist']
+                     )
+    errors = np.sqrt(np.diag(covariance))
+    print(params)
+    print(errors)
 
-    # params, covariance = curve_fit(linear_fit,
-    #                  days,
-    #                  df_components.loc[df_components['c_i'] == i, 'radial_dist']
-    #                  )
-    # errors = np.sqrt(np.diag(covariance))
-    #
-    # plt.plot(mdates.num2date(x_values), linear_fit(x_values, *params), linewidth=1.5, color='black', linestyle='--')
+    plt.plot(np.array(x_values, dtype='datetime64[D]'), linear_fit(x_values, *params), linewidth=0.5, color=colors[i], linestyle='--')
+    plt.show()
 
-
-
-# df['date_delta'] = (df['date'] - df['date'].min())  / np.timedelta64(1,'D')
-# y = df_components['radial_dist']
-#
-# params, covariance = curve_fit(linear_fit,
-#                      x1,
-#                      y
-#                      )
-# errors = np.sqrt(np.diag(covariance))
-#
-# print(params)
 
 plt.ylabel('Distance / mas')
 plt.xlabel('Date')
-#plt.ylim(-1,13)
-#plt.legend(loc=9, ncol=5)
-#plt.tight_layout()
+plt.xlim(np.array(x_values, dtype='datetime64[D]')[0],np.array(x_values, dtype='datetime64[D]')[-1])
+plt.ylim(-1,13)
+plt.ylim()
+plt.legend(loc=9, ncol=5)
+plt.tight_layout()
 plt.savefig('jet_motion.pdf', bbox_inches='tight', pad_inches=0.1)
-plt.close()
