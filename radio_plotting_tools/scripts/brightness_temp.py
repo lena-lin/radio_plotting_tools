7438129adadfed5b87a06ff8878732ec69b40736
Kevin Schmidt <kevin3.schmidt@udo.edu>
2018-02-13 12:13:20 +0100

    made a function

--- radio_plotting_tools/scripts/brightness_temp.py
+++ radio_plotting_tools/scripts/brightness_temp.py
@@ -5,25 +5,33 @@ import uncertainties.unumpy as unp
 import scipy.constants as const
 import astropy.units as u
 
-df_components = pd.read_csv('components.csv')
-
-lamb = const.c / 15e9
-kB = unp.uarray(const.physical_constants['Boltzmann constant'][0], const.physical_constants['Boltzmann constant'][2])
-S = (df_components.loc[df_components['c_i'] == 0, 'flux'])
-z = 0.078
-a_maj = u.mas.to(u.rad, df_components.loc[df_components['c_i'] == 0, 'major_axes'])
-a_min = u.mas.to(u.rad, df_components.loc[df_components['c_i'] == 0, 'minor_axes'])
 
 
-bright_temp = (2 * np.log(2) / (np.pi * kB)) * ( (S*1e-26 * lamb**2 * (1 + z)) / ( a_maj * a_min ))
+def brightness_temp(
+    input_file,
+    loglevs,
+):
+    df_components = pd.read_csv('components.csv')
 
-df_bright_temp = pd.DataFrame({'bright_temp': bright_temp})
+    lamb = const.c / 15e9
+    kB = unp.uarray(const.physical_constants['Boltzmann constant'][0], const.physical_constants['Boltzmann constant'][2])
+    S = (df_components.loc[df_components['c_i'] == 0, 'flux'])
+    z = 0.078
+    a_maj = u.mas.to(u.rad, df_components.loc[df_components['c_i'] == 0, 'major_axes'])
+    a_min = u.mas.to(u.rad, df_components.loc[df_components['c_i'] == 0, 'minor_axes'])
 
-print(df_bright_temp)
 
-pd.concat([df_components, df_bright_temp], axis=1)
+    bright_temp = (2 * np.log(2) / (np.pi * kB)) * ( (S*1e-26 * lamb**2 * (1 + z)) / ( a_maj * a_min ))
 
-print(df_components)
+    df_bright_temp = pd.DataFrame({'bright_temp': bright_temp})
+
+    df_bright_temp.to_csv('bright_temp.csv')
+
+
+
+if __name__ == '__main__':
+    main()
+
 
 
 '''
