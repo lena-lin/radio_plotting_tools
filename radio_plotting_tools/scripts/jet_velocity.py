e57c4d262b849f2710af430794a2e3a6d8eae394
Kevin Schmidt <kevin3.schmidt@udo.edu>
2018-02-13 13:09:38 +0100

    made a function

--- radio_plotting_tools/scripts/jet_velocity.py
+++ radio_plotting_tools/scripts/jet_velocity.py
@@ -17,90 +17,94 @@ from datetime import datetime
 import uncertainties.unumpy as unp
 import scipy.constants as const
 
-#################################################################################################################################################
+def jet_velocity(
+    input_file,
+    line_of_sight
+):
+    # Load data / array with all observation dates
+    df_components = pd.read_csv(input_file)
+    dates = np.unique(df_components['date'])
 
-# Load data / array with all observation dates
-df_components = pd.read_csv('components.csv')
-dates = np.unique(df_components['date'])
+    # Add delta_days to df
+    date = (pd.to_datetime(df_components['date'], format='%Y-%m-%d'))
+    delta_days = ((date - date.min()) / np.timedelta64(1,'D'))
+    delta_days = pd.DataFrame({'delta_days': delta_days})
+    df_components = pd.concat([df_components, delta_days], axis=1)
 
-# Add delta_days to df
+    # Define linear function
+    def linear_fit(f, m, b):
+        return m * f + b
 
-date = (pd.to_datetime(df_components['date'], format='%Y-%m-%d'))
-delta_days = ((date - date.min()) / np.timedelta64(1,'D'))
-delta_days = pd.DataFrame({'delta_days': delta_days})
-df_components = pd.concat([df_components, delta_days], axis=1)
+    x_values = np.linspace(-10, 1000, 10)
 
-# Define linear function
-def linear_fit(f, m, b):
-    return m * f + b
+    # Colors for different c_i
+    colors = (['red', 'blue', 'magenta', 'orange', 'green', 'brown', 'cyan', 'darkorange', 'grey', 'olive'])
 
-x_values = np.linspace(-10, 1000, 10)
+    # Create df for velocities
+    df_velocities = pd.DataFrame(columns=['component', 'velocity', 'velocity_err'])
 
-# Colors for different c_i
-colors = (['red', 'blue', 'magenta', 'orange', 'green', 'brown', 'cyan', 'darkorange', 'grey', 'olive'])
+    #################################################################################################################################################
 
-df_velocities = pd.DataFrame(columns=['component', 'velocity', 'velocity_err'])
+    # Plot radial_dist vs dates
+    fig = plt.figure(figsize=(12, 10))
+    ax = fig.add_subplot(111)
 
-#################################################################################################################################################
+    for i in range(len(df_components.loc[df_components['date'] == '2013-12-15', 'x_positions'])):
+        plt.errorbar(df_components.loc[df_components['c_i'] == i, 'delta_days'],
+                 df_components.loc[df_components['c_i'] == i, 'radial_dist'],
+                 yerr = (df_components.loc[df_components['c_i'] == i, 'major_axes'])/2,
+                 linestyle='none',
+                 marker='.',
+                 markersize=8,
+                 label='C_'+str(i),
+                 color=colors[i]
+                 )
 
-# Plot radial_dist vs dates
-fig = plt.figure(figsize=(12, 10))
-ax = fig.add_subplot(111)
+        params, covariance = curve_fit(linear_fit,
+                         df_components.loc[df_components['c_i'] == i, 'delta_days'],
+                         df_components.loc[df_components['c_i'] == i, 'radial_dist'],
+                         sigma = (df_components.loc[df_components['c_i'] == i, 'major_axes'])/2
+                         )
+        errors = np.sqrt(np.diag(covariance))
 
-for i in range(len(df_components.loc[df_components['date'] == '2013-12-15', 'x_positions'])):
-    plt.errorbar(df_components.loc[df_components['c_i'] == i, 'delta_days'],
-             df_components.loc[df_components['c_i'] == i, 'radial_dist'],
-             yerr = (df_components.loc[df_components['c_i'] == i, 'major_axes'])/2,
-             linestyle='none',
-             marker='.',
-             markersize=8,
-             label='C_'+str(i),
-             color=colors[i]
-             )
+        component = str(i)
+        velocity = params[0] * 365
+        velocity_err = errors[0] * 365
 
-    params, covariance = curve_fit(linear_fit,
-                     df_components.loc[df_components['c_i'] == i, 'delta_days'],
-                     df_components.loc[df_components['c_i'] == i, 'radial_dist'],
-                     sigma = (df_components.loc[df_components['c_i'] == i, 'major_axes'])/2
-                     )
-    errors = np.sqrt(np.diag(covariance))
+        df_velocities_i = pd.DataFrame({'component': component,
+                                  'velocity': velocity,
+                                  'velocity_err': velocity_err
+                                  }, index=[i])
+        df_velocities = pd.concat([df_velocities, df_velocities_i], ignore_index=True)
 
-    component = str(i)
-    velocity = params[0] * 365
-    velocity_err = errors[0] * 365
+        plt.plot(x_values, linear_fit(x_values, *params), linewidth=0.5, color=colors[i], linestyle='--')
 
-    df_velocities_i = pd.DataFrame({'component': component,
-                              'velocity': velocity,
-                              'velocity_err': velocity_err
-                              }, index=[i])
-    df_velocities = pd.concat([df_velocities, df_velocities_i], ignore_index=True)
+    plt.ylabel('Distance / mas')
+    plt.xlabel('Date')
+    plt.xticks(np.unique(df_components['delta_days']), np.unique(df_components['date']), rotation=45)
+    plt.xlim(-10, 1000)
+    plt.ylim(-1,13)
+    plt.ylim()
+    plt.legend(loc=9, ncol=4)
+    plt.tight_layout()
+    plt.savefig('jet_velocity.pdf', bbox_inches='tight', pad_inches=0.1)
 
-    plt.plot(x_values, linear_fit(x_values, *params), linewidth=0.5, color=colors[i], linestyle='--')
+    #################################################################################################################################################
 
-plt.ylabel('Distance / mas')
-plt.xlabel('Date')
-plt.xticks(np.unique(df_components['delta_days']), np.unique(df_components['date']), rotation=45)
-plt.xlim(-10, 1000)
-plt.ylim(-1,13)
-plt.ylim()
-plt.legend(loc=9, ncol=4)
-plt.tight_layout()
-plt.savefig('jet_velocity.pdf', bbox_inches='tight', pad_inches=0.1)
+    # Determine apparent velocities and save to df
+    phi_agn = line_of_sight
+    v_app = unp.uarray(df_velocities['velocity'], df_velocities['velocity_err'])
+    c = const.c
 
-#################################################################################################################################################
+    beta = v_app / c
+    beta_app = ( beta * np.sin(phi_agn) ) / ( 1 - beta * np.cos(phi_agn) )
 
-# Determine apparent velocities and save to df
-phi_agn = np.deg2rad(10)
-v_app = unp.uarray(df_velocities['velocity'], df_velocities['velocity_err'])
-c = const.c
+    df_beta = pd.DataFrame({'beta_app': beta_app})
 
-beta = v_app / c
-beta_app = ( beta * np.sin(phi_agn) ) / ( 1 - beta * np.cos(phi_agn) )
+    df_velocities = pd.concat([df_velocities, df_beta], axis=1)
 
-df_beta = pd.DataFrame({'beta_app': beta_app,
-                        })
+    df_velocities.to_csv('component_velocities.csv')
 
-df_velocities = pd.concat([df_velocities, df_beta], axis=1)
 
-print(df_velocities)
-df_velocities.to_csv('component_velocities.csv')
+if __name__ == '__main__':
+  jet_velocity()
