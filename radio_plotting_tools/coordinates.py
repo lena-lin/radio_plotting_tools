31e4580740fd66a86ca90635a87387908f593696
llinhoff <lena.linhoff@tu-dortmund.de>
2018-08-17 12:39:57 +0200

    add reference pixel shift

--- radio_plotting_tools/coordinates.py
+++ radio_plotting_tools/coordinates.py
@@ -1,21 +1,29 @@
 import astropy.units as u
 import numpy as np
 
-def get_pixel_coordinates(header, shift_x_ref=0, shift_y_ref=0, relative=True):
+
+def get_pixel_coordinates(header, x_ref_pixel=None, y_ref_pixel=None, relative=True):
     x_n_pixel = header['NAXIS1']
-    x_ref_pixel = header['CRPIX1']
+
     x_inc = (header['CDELT1'] * u.degree).to(u.mas)
     x_ref_value = (header['CRVAL1'] * u.degree).to(u.mas)
     y_n_pixel = header['NAXIS2']
-    y_ref_pixel = header['CRPIX2']
+
     y_inc = (header['CDELT2'] * u.degree).to(u.mas)
     y_ref_value = (header['CRVAL2'] * u.degree).to(u.mas)
 
-    x = np.linspace(x_ref_pixel * x_inc, -x_ref_pixel * x_inc, x_n_pixel)
-    y = np.linspace(-y_ref_pixel * y_inc, y_ref_pixel * y_inc, y_n_pixel)
+    if x_ref_pixel == None:
+        x_ref_pixel = header['CRPIX1']
+    if y_ref_pixel == None:
+        y_ref_pixel = header['CRPIX2']
+
+    print(x_ref_pixel)
+
+    x = np.linspace(x_ref_pixel * -x_inc, (x_n_pixel - x_ref_pixel) * x_inc, x_n_pixel)
+    y = np.linspace(y_ref_pixel * -y_inc, (y_n_pixel - y_ref_pixel) * y_inc, y_n_pixel)
 
     if not relative:
-        x -= x_ref_value
-        y -= y_ref_value
+        x += x_ref_value
+        y += y_ref_value
 
     return x, y
