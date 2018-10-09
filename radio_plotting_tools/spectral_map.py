20360b29e43b987df0dd81b07df0f3bf2731c4d9
llinhoff <lena.linhoff@tu-dortmund.de>
2018-10-09 16:49:47 +0200

    move  find peak functions to coordinates

--- radio_plotting_tools/spectral_map.py
+++ radio_plotting_tools/spectral_map.py
@@ -4,7 +4,7 @@ from astropy.io import fits
 from astropy.convolution import Gaussian2DKernel, convolve
 from scipy import interpolate
 from .coordinates import get_pixel_coordinates
-from skimage.feature import peak_local_max
+
 from radio_plotting_tools.noise import noise_level
 
 
@@ -29,40 +29,7 @@ def interpolated_map(header, clean_map, offset, ra=[5, -5], dec=[-5, 5], convolu
     return ra_linspace, dec_linspace, func_interpol_cm(ra_linspace, dec_linspace)
 
 
-def find_peaks(file, c1_x_ref=1054, c1_y_ref=1026, sigma=3000):
-    header = fits.open(file)[0].header
-    clean_map = fits.open(file)[0].data[0][0]
 
-    threshold = sigma * header['NOISE']
-    x_ref = header['CRPIX1']
-    y_ref = header['CRPIX2']
-
-    peaks = peak_local_max(clean_map, min_distance=10, threshold_abs=threshold)
-
-    dist = 1000
-    for peak in peaks:
-        if abs(np.subtract(peak, [c1_x_ref, c1_y_ref])).sum() < dist:
-            x_ref, y_ref = peak
-            dist = abs(np.subtract(peak, [c1_x_ref, c1_y_ref])).sum()
-
-    return y_ref, x_ref
-
-
-def find_peaks_max_y(header, clean_map, max_y_offset, sigma=150):
-    threshold = sigma * header['NOISE']
-    x_ref = header['CRPIX1']
-    y_ref = header['CRPIX2']
-
-    x_header = x_ref
-    y_header = y_ref
-    peaks = peak_local_max(clean_map, min_distance=5, threshold_abs=threshold)
-    max_y = y_ref
-    for peak in peaks:
-        if (peak[0] > max_y) and (abs(peak[0] - y_header) < max_y_offset) and (abs(peak[1] - x_header) < 10):
-            max_y = peak[0]
-            y_ref, x_ref = peak
-
-    return x_ref, y_ref
 
 
 def beamsize(header):
